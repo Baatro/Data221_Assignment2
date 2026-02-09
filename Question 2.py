e7aa2e9cb358219948249c938730399001268015
KevinPham <kevinpp2007@gmail.com>
2026-02-09 10:30:14 -0700

    Correct lowercase conversion and clean up code
    Fixed token conversion to lowercase and removed bigram generation and counting logic.

--- Question 2.py
+++ Question 2.py
@@ -7,7 +7,7 @@ with open('sample-file.txt', 'r') as file:
 tokens_unfiltered = text.split()
 
 # Convert to lowercase
-tokens_unfiltered = [token_unfiltered.lower() for token in tokens_unfiltered]
+tokens_unfiltered = [tokens_unfiltered.lower() for token in tokens_unfiltered]
 
 # Remove punctuation from beginning and end
 for i in range(len(tokens_unfiltered)):
@@ -19,46 +19,3 @@ for word in tokens_unfiltered:
         clean_tokens.append(word)
 
 tokens = clean_tokens
-    
-
-bigrams = []
-# Create bigrams
-for i in range(len(tokens) - 1):
-    bigram = (tokens[i], tokens[i + 1])
-    bigrams.append(bigram)
-
-# Count bigrams
-bigram_counts = {}
-for bigram in bigrams:
-    bigram_counts[bigram] = bigram_counts.get(bigram, 0) + 1
-
-# Sort and find top 5
-sorted_bigrams = []
-bigram_counts_copy = bigram_counts.copy()
-for i in range(5):
-    if len(bigram_counts_copy) == 0:
-        break
-    max_bigram = None
-    max_count = 0
-    for bigram in bigram_counts_copy:
-        if bigram_counts_copy[bigram] > max_count:
-            max_count = bigram_counts_copy[bigram]
-            max_bigram = bigram
-    if max_bigram is not None:
-        sorted_bigrams.append((max_bigram, max_count))
-        del bigram_counts_copy[max_bigram]
-
-# Print top 5
-print("Top 5 most common bigrams:")
-counter = 1
-for bigram_tuple in sorted_bigrams:
-    bigram = bigram_tuple[0]
-    count = bigram_tuple[1]
-    print(f"{counter}. {bigram}: {count}")
-    counter = counter + 1
-
-
-
-
-
-
