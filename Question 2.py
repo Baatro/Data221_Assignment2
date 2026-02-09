dab0f74b6e211cd69e36919e77d4d8a0082784c5
KevinPham <kevinpp2007@gmail.com>
2026-02-09 10:08:46 -0700

    Enhance token processing and filtering
    Added filtering to remove tokens with less than 2 letters and adjusted token processing.

--- Question 2.py
+++ Question 2.py
@@ -4,14 +4,22 @@
 with open('sample-file.txt', 'r') as file:
     text = file.read()
 # Split into tokens
-tokens = text.split()
+tokens_unfiltered = text.split()
 
 # Convert to lowercase
-tokens = [token.lower() for token in tokens]
+tokens_unfiltered = [token_unfiltered.lower() for token in tokens_unfiltered]
 
 # Remove punctuation from beginning and end
-for i in range(len(tokens)):
-    tokens[i] = tokens[i].strip('.,!?";():')
+for i in range(len(tokens_unfiltered)):
+    tokens_unfiltered[i] = tokens_unfiltered[i].strip('.,!?";():')
+# check for 2 letters or more
+clean_tokens = []
+for word in tokens_unfiltered:
+    if len(word) >= 2:
+        clean_tokens.append(word)
+
+tokens = clean_tokens
+    
 
 bigrams = []
 # Create bigrams
